a86a3192a6874929a2fd08b2bcb84b535c2506a4
Facundo Weihmüller <167568373+FacuW@users.noreply.github.com>
2024-08-12 16:17:00 -0300

    Add files via upload

---
+++ TAREA1.c
@@ -0,0 +1,20 @@
+#include <stdio.h> 
+
+int main(int argc, char** argv)
+{
+    
+    printf("Primera tarea del cursado:\n");
+
+    int numero = 6;
+
+    if(numero >= 6)
+    {
+       printf("tarea completada\n"); 
+    }
+    else
+    {
+       printf("insuficiente\n");
+    }
+
+    return 0;
+}
